bf38091bd7653eb3ca8fb0580a5994068813a9ad
Mohammed Foud <mfoud444@gmail.com>
2024-10-16 00:35:41 +0300

    first commit

---
+++ randapi/randai/service/__init__.py
@@ -0,0 +1,3 @@
+from .chat_text import ChatText
+from .image_generator import ImageGenerator
+from .research_gen import ResearchGen
---
+++ randapi/randai/util/__init__.py
@@ -0,0 +1,4 @@
+from .Settings import Settings
+from .helper import Helper
+from .translate import TextTran
+from .Document import  PandocConverter, WordTool
